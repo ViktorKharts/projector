60b8acecf7cbf9d8c1dfd0a086670b881403700c
viktor.k <viktor.kharts@gmail.com>
2025-07-12 18:43:25 +0200

    feat(add-p): add create new project;

---
+++ cmd/add-project.go
@@ -0,0 +1,94 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(addProjectCmd)
+}
+
+var addProjectCmd = &cobra.Command{
+	Use:     "add-p",
+	Short:   "Creates a new project.",
+	Long:    "Add a new project to the list of ongoing initiatives.",
+	Aliases: []string{"ap", "addp", "add-project", "np", "new-project", "newp"},
+	Run:     addProject,
+}
+
+func addProject(cmd *cobra.Command, args []string) {
+	var fd FileData
+
+	pName := args[0]
+	if len(args) == 0 {
+		pName = "New Project"
+	}
+
+	p := Project{
+		Name: pName,
+		Tasks: []Task{
+			{
+				Value:      fmt.Sprintf("Project %s initiated.", args[0]),
+				isComplete: false,
+			},
+		},
+	}
+
+	storage := os.Getenv("HOME") + "/projector-storage"
+	f, err := os.ReadFile(storage)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			_, err := os.Create(storage)
+			if err != nil {
+				fmt.Printf("Projector Error: failed to create a storage file.\n%s\n", err.Error())
+				os.Exit(1)
+			}
+			list(cmd, args)
+		} else {
+			fmt.Printf("Projector Error: failed to read storage file.\n%s\n", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	if len(f) == 0 {
+		fd.SelectedProject = p.Name
+		fd.Projects = append(fd.Projects, p)
+
+		bd, err := json.Marshal(fd)
+		if err != nil {
+			fmt.Printf("Projector Error: failed to Marshal Project data before save.\n%s\n", err.Error())
+			os.Exit(1)
+		}
+
+		if err := os.WriteFile(storage, bd, 0666); err != nil {
+			fmt.Printf("Projector Error: failed to write Project data into storage file.\n%s\n", err.Error())
+			os.Exit(1)
+		}
+		return
+	}
+
+	if err = json.Unmarshal(f, &fd); err != nil {
+		fmt.Printf("Projector Error: failed to parse file byte data into json.\n%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	fd.SelectedProject = p.Name
+	fd.Projects = append(fd.Projects, p)
+
+	bd, err := json.Marshal(fd)
+	if err != nil {
+		fmt.Printf("Projector Error: failed to Marshal Project data before save.\n%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	if err := os.WriteFile(storage, bd, 0666); err != nil {
+		fmt.Printf("Projector Error: failed to write Project data into storage file.\n%s\n", err.Error())
+		os.Exit(1)
+	}
+}
